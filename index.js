a75b7d672a70093e5c1e9f28e21ada55db826692
Mariana Ramírez <marianasmatos1@gmail.com>
2023-11-29 10:31:35 -0500

    Update1 index.js

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// My Code
+let dataBaseUrl = {}
+let nextUrl = 1;
+
+app.post('/api/shorturl', function(req, res) {
+  const original_url = req.body.original_url;
+
+  nextUrl +1;
+  dataBaseUrl[nextUrl] = original_url;
+  const short_url = nextUrl;
+  res.json({original_url, short_url})
+});
+
+app.post('/api/shorturl/shorturl', function(req, res) {
+  const url = req.body.original_url;
+  if (dataBaseUrl.hasOwnProperty(shortUrl)) {
+    res.redirect(url);
+  } else {
+    res.json({error: 'invalid url'});
+  }
+  res.redirect(url);
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
